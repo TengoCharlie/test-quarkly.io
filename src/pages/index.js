eb6ddb5456151cd9760a68b7a42be361f7c7a249
Harsh Tenguriya <32489875+TengoCharlie@users.noreply.github.com>
2020-12-28 13:46:15 +0530

    Changes

---
+++ src/pages/index.js
@@ -0,0 +1,93 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, StackItem, Stack, Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						margin-right="-20%"
+						padding-bottom="120%"
+						background="url(https://images.unsplash.com/photo-1609058745811-2e87ab15790a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&h=2000) 50% 15%/cover"
+						lg-margin-right="0"
+						lg-padding-bottom="80%"
+					/>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						padding="98px 64px"
+						margin-left="-20%"
+						margin-top="36px"
+						background="--color-red"
+						color="--light"
+						mix-blend-mode="lighten"
+						lg-margin-left="0"
+						lg-margin-top="0"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="0"
+						font="--base"
+						letter-spacing="1px"
+						text-transform="uppercase"
+					>
+						About me
+					</Text>
+					<Text
+						as="h1"
+						margin="0 0 16px 0"
+						font="--headline1"
+						lg-font="--headline2"
+						sm-font="normal 900 25px/1.2 -apple-system, system-ui, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"
+					>
+						Hey I’m David
+					</Text>
+					<Text
+						as="p"
+						margin="16px 0"
+						font="--base"
+						max-width="400px"
+						sm-text-align="justify"
+					>
+						My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
