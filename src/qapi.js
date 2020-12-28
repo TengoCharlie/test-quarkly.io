9b5e4aacc8c3c28b71cb5f44ee7c8869ff7f074b
Harsh Tenguriya <32489875+TengoCharlie@users.noreply.github.com>
2020-12-28 13:42:14 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe9929aa6aceb001f31fe85",
+				"5fe9929aa6aceb001f31fe87"
+			]
+		},
+		"5fe9929aa6aceb001f31fe85": {
+			"id": "5fe9929aa6aceb001f31fe85",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe9929aa6aceb001f31fe87": {
+			"id": "5fe9929aa6aceb001f31fe87",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
